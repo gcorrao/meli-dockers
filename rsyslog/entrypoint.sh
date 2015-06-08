bbbe53ae0070e87538de58a9793040d461b156ee
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2015-06-08 10:57:13 -0300

    add basic rsyslog image

---
+++ rsyslog/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#set -e
+
+if [[ $APPLICATION ]]; then
+  sed -i -e "s/##APPLICATION##/${APPLICATION}/" "/etc/rsyslog.d/10-docker.conf"
+else
+  echo "You must set APPLICATION environment variable"
+  exit 1
+fi
+
+if [[ $SCOPE ]]; then
+  sed -i -e "s/##SCOPE##/${SCOPE}/" "/etc/rsyslog.d/10-docker.conf"
+else
+  echo "You must set SCOPE environment variable"
+  exit 1
+fi
+
+if [[ $VERSION ]]; then
+  sed -i -e "s/##VERSION##/${VERSION}/" "/etc/rsyslog.d/10-docker.conf"
+else
+  echo "You must set VERSION environment variable"
+  exit 1
+fi
+
+exec "$@"
