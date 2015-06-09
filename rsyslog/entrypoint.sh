d97fa961cda350649f7fdb8bf1799934c4893aac
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2015-06-09 11:25:29 -0300

    reorganize dockerfile, remove rsyslog docker config file, merge it with main file, add rfc syslog parsing to rsyslog config

--- rsyslog/entrypoint.sh
+++ rsyslog/entrypoint.sh
@@ -2,26 +2,26 @@
 #set -e
 
 if [[ $APPLICATION ]]; then
-  sed -i -e "s/##APPLICATION##/${APPLICATION}/" "/etc/rsyslog.d/10-docker.conf"
+  sed -i -e "s/##APPLICATION##/${APPLICATION}/" "/etc/rsyslog.conf"
 else
   echo "You must set APPLICATION environment variable"
   exit 1
 fi
 
 if [[ $SCOPE ]]; then
-  sed -i -e "s/##SCOPE##/${SCOPE}/" "/etc/rsyslog.d/10-docker.conf"
+  sed -i -e "s/##SCOPE##/${SCOPE}/" "/etc/rsyslog.conf"
 else
   echo "You must set SCOPE environment variable"
   exit 1
 fi
 
 if [[ $VERSION ]]; then
-  sed -i -e "s/##VERSION##/${VERSION}/" "/etc/rsyslog.d/10-docker.conf"
+  sed -i -e "s/##VERSION##/${VERSION}/" "/etc/rsyslog.conf"
 else
   echo "You must set VERSION environment variable"
   exit 1
 fi
 
-sed -i -e "s/##HOST##/$(hostname)/" "/etc/rsyslog.d/10-docker.conf"
+sed -i -e "s/##HOST##/$(hostname)/" "/etc/rsyslog.conf"
 
 exec "$@"
